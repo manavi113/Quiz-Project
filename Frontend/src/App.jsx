442c504c7da772d54628a81086e34482164b954e
Manavi Srivastava <srivastavamanavi168@gmail.com>
2025-05-25 02:00:18 -0400

    Coe Refinement

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,7 @@
 import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+ 
 import './App.css'
-// import { Quiz } from './Components/Quiz'
-// import { Home } from './Components/Home'
+ 
 import QuizApp from './Components/QuizApp'
 
 function App () {
--- Frontend/src/Components/ScoreCard.jsx
+++ Frontend/src/Components/ScoreCard.jsx
@@ -1,35 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import '../ScoreCard.css'
 import {facts} from '../Facts'
- 
-
-// export const ScoreCard = ({score, name}) => {
-//   const startagain =()=>{
-//    window.location.reload();   
-//   }
-//   return (
-//     <>
-//     <div className='card' style={{display:'flex', flexDirection:'column'}}>
-//         <div className="score-container">
-//                 <h4 className='name'>Name:{name}</h4>
-//                 <h4 className='topic'>General Knowledge</h4>
-//                    <h2>SCORE:{score}/10</h2>
-    
-//              </div>
-      
-//     </div>
-//     <button style={{width:'100%', background:'green', marginTop:24}} onClick={startagain}>Start Again</button>
-//     </>
-//   )
-// }
-
-
-
-
-
-
-
-
 export const ScoreCard = ({ score, name }) => {
   const startagain = () => {
     window.location.reload();
